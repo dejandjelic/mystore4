6e724e4a43c2ae4619416642cb17a7d586f8683a
Studio <Studio@DESKTOP-IGKDVVT>
2023-03-22 12:25:24 +0100

    Add project files.

---
+++ Pages/Clients/delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mystore3.Pages.Clients
+{
+    public class deleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
